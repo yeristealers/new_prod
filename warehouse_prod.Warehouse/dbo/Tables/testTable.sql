2a5e04218e0cf640235fb1c7a52c636d2c18d787
yeristealers <143675018+yeristealers@users.noreply.github.com>
2026-06-09 10:39:41 +0900

    Committing 9 items from workspace f996843e-4b6c-4eab-9469-cb46b1a960c5

---
+++ warehouse_prod.Warehouse/dbo/Tables/testTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[testTable] (
+
+	[countryOrRegion] varchar(8000) NULL, 
+	[holidayName] varchar(8000) NULL, 
+	[normalizeHolidayName] varchar(8000) NULL, 
+	[isPaidTimeOff] bit NULL, 
+	[countryRegionCode] varchar(8000) NULL, 
+	[date] datetime2(6) NULL
+);
---
+++ warehouse_prod.Warehouse/dbo/Tables/testTable_1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[testTable_1] (
+
+	[countryOrRegion] varchar(8000) NULL, 
+	[holidayName] varchar(8000) NULL, 
+	[normalizeHolidayName] varchar(8000) NULL, 
+	[isPaidTimeOff] bigint NULL, 
+	[countryRegionCode] varchar(8000) NULL, 
+	[date] datetime2(6) NULL, 
+	[BATCH_IDENTIFIER] varchar(8000) NULL, 
+	[Account Level 3 + Name] datetime2(6) NULL
+);
